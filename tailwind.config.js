1febeed67d1257b288a054d7c4db1704595cb423
nnekasandra <nnekasandra@gmail.com>
2021-12-11 11:30:34 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('public/images/bg-dots.png')",
+       },
+       colors: {
+        'dark-black': '#2c2f34',
+        'text-colors': '#00adef',
+        'hover': '#08f',
+        'light': '#1f2024',
+        'darker': '#2c2e32',
+        'lighter': '#ffffff1a',
+        'light-white': '#ccc',
+        'slant-color': '#00adef'
+      }
+    },
+    container:{
+      center: true,
+      padding: '2rem'
+    },
+    image:{
+      url: "public/images/bg-dots.png"
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
