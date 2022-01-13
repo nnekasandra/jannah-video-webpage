e4b79759792e5a9aabfceccd374fb5b32bd42511
nnekasandra <nnekasandra@gmail.com>
2022-01-13 17:14:21 +0100

    fixed footer section made grids more responsive and better rounded curves

--- public/index.js
+++ public/index.js
@@ -1,6 +1,12 @@
-let followBtn = document.getElementById('follow');
-let smLinks = document.getElementById('sm-links');
-followBtn.addEventListener('mousemove',function(){
-    smLinks.style.visibility = 'visible';
-    followBtn.appendChild(smLinks);
-})
+let scrollTopBtn = document.getElementById("top");
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopBtn.style.display = "block";
+      } else {
+        scrollTopBtn.style.display = "none";
+      }
+};
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
